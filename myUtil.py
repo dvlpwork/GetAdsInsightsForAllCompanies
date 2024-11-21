0256e479b1bb8891ec55a2530d22dd5801872fa6
mu5dvlp <mu5dvlp@gmail.com>
2024-11-21 17:55:56 +0900

    タイムゾーン考慮

--- myUtil.py
+++ myUtil.py
@@ -1,11 +1,17 @@
 from datetime import datetime, timedelta
+import pytz
+
 
 def getToday():
-    today = datetime.now()
+    jst = pytz.timezone("Asia/Tokyo")
+    today = datetime.now(jst)
     today_str = today.strftime("%Y-%m-%d")
     return today_str
 
+
 def getYesterday():
-    yesterday = datetime.now() - timedelta(days=1)
+    jst = pytz.timezone("Asia/Tokyo")
+    now_jst = datetime.now(jst)
+    yesterday = now_jst - timedelta(days=1)
     yesterday_str = yesterday.strftime("%Y-%m-%d")
     return yesterday_str
